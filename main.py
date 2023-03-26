1952c62add698950fb49875bacd54e7df2ffb564
poclab <gotoh-hiroaki-yw@ynu.ac.jp>
2023-03-26 14:53:47 +0900

    environmentの更新

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from io import StringIO
 
 import openbabel
 from openbabel import pybel
@@ -12,14 +13,31 @@ st.text(len(pybel.outformats))
 
 import subprocess
 
+uploaded_file = st.file_uploader("datファイルをアップロードしてください")
+
+st.text(uploaded_file)
+if uploaded_file is not None:
+
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+    # calc_file = st.write(bytes_data)
+    with open("test.dat", mode='w') as f:
+        f.write(stringio.read())
+
+
 if st.button('計算開始'):
-    file_path = "phenol.dat"
+    file_path = "test.dat"
     # mopacの後にスペースを1つ開ける。
     result = subprocess.run('mopac ' + file_path, shell=True)
     st.text(result)
 
+import os
+
+file_name = os.path.splitext(uploaded_file.name)[0]
+st.text(file_name + ".arc")
+
 if st.button('ファイルの読み取り'):
-    with open('phenol.arc', mode='r') as f:
+    with open("test.arc", mode='r') as f:
         lines = f.readlines()
         for line in lines:
             st.text(line)
